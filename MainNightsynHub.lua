03077dbe7f36c9c7a5d2e12c8d626940864f8b69
Nightcorepro <abhijeetmandal2050@gmail.com>
2026-02-27 09:49:59 +0530

    Fix syntax error in loadstring function

--- MainNightsynHub.lua
+++ MainNightsynHub.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 2753915549 or game.PlaceId == 79091703265657 or game.PlaceId == 100117331123089 then --Place IDs for Blox Fruits
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Nightcorepro/NightSyn-Hub/refs/heads/main/Sea1.lua")()
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/Nightcorepro/NightSyn-Hub/refs/heads/main/Sea1.lua")()
 else
     game.Players.LocalPlayer:Kick("This script is only for Blox Fruits")
     return
