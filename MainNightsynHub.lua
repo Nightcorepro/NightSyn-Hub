60a780e9f056da4286222d730b838523655b2461
Nightcorepro <abhijeetmandal2050@gmail.com>
2026-02-27 11:59:28 +0530

    Refactor Place ID checks for Blox Fruits script

--- MainNightsynHub.lua
+++ MainNightsynHub.lua
@@ -1,5 +1,9 @@
-if game.PlaceId == 2753915549 or game.PlaceId == 79091703265657 or game.PlaceId == 100117331123089 then --Place IDs for Blox Fruits
+if game.PlaceId == 2753915549 then --Place ID for Sea1
     loadstring(game:HttpGet"https://raw.githubusercontent.com/Nightcorepro/NightSyn-Hub/refs/heads/main/Sea1.lua")()
+elseif game.PlaceId == 79091703265657 then --Place ID for Sea2
+    return
+elseif game.PlaceId == 100117331123089 then --Place ID for Sea3
+    return
 else
     game.Players.LocalPlayer:Kick("This script is only for Blox Fruits")
     return
